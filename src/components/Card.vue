c3473895fd074e8a07d62cd02f2816a2455c86f5
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2023-11-04 17:10:41 +0500

    update

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,21 +2,21 @@
     <div class="container mx-auto grid gap-10 md:gap-14 lg:gap-[100px]">
         <div class="grid md:flex justify-center items-center gap-10 md:gap-6">
             <div class="w-[350px] md:w-[350px] lg:w-[600px] 2xl:w-[730px] mx-auto">
-                <img src="../assets/images/project/kitchen.png" alt="" class="rounded-t-xl border-4 border-[#34B8A3]">
+                <img src="../assets/images/projects/kitchen.png" alt="" class="rounded-t-xl border-4 border-[#34B8A3]">
                 <h5 class="mb-2 text-[20px] md:text-[24px] lg:text-[32px] h-[30px] font-[500] tracking-tight text-white mt-3">{{$t('translation.portfolio.designTitle1')}}</h5>
             </div>
             <div class="w-[350px] md:w-[350px] lg:w-[600px] 2xl:w-[730px] mx-auto">
-                <img src="../assets/images/project/shifo.png" alt="" class="rounded-t-xl border-4 border-[#34B8A3]">
+                <img src="../assets/images/projects/shifo.png" alt="" class="rounded-t-xl border-4 border-[#34B8A3]">
                 <h5 class="mb-2 text-[20px] md:text-[24px] lg:text-[32px] h-[30px] font-[500] tracking-tight text-white mt-3">{{$t('translation.portfolio.designTitle2')}}</h5>
             </div>
         </div>
         <div class="grid md:flex justify-center items-center gap-10 md:gap-6">
             <div class="w-[350px] md:w-[350px] lg:w-[600px] 2xl:w-[730px] mx-auto">
-                <img src="../assets/images/project/shifoadmin.png" alt="" class="rounded-t-xl border-4 border-[#34B8A3]">
+                <img src="../assets/images/projects/shifoadmin.png" alt="" class="rounded-t-xl border-4 border-[#34B8A3]">
                 <h5 class="mb-2 text-[20px] md:text-[24px] lg:text-[32px] h-[30px] font-[500] tracking-tight text-white mt-3">{{$t('translation.portfolio.designTitle3')}}</h5>
             </div>
             <div class="w-[350px] md:w-[350px] lg:w-[600px] 2xl:w-[730px] mx-auto">
-                <img src="../assets/images/project/barbershop.png" alt="" class="rounded-t-xl border-4 border-[#34B8A3]">
+                <img src="../assets/images/projects/barbershop.png" alt="" class="rounded-t-xl border-4 border-[#34B8A3]">
                 <h5 class="mb-2 text-[20px] md:text-[24px] lg:text-[32px] h-[30px] font-[500] tracking-tight text-white mt-3">{{$t('translation.portfolio.designTitle4')}}</h5>
             </div>
         </div>
