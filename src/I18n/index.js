38557945d41bd1551d4e84072e637936ab3141dc
Elyor Usmanov <94918957+masterusmanov@users.noreply.github.com>
2023-11-06 11:57:20 +0500

    update

--- src/I18n/index.js
+++ src/I18n/index.js
@@ -2,7 +2,7 @@ import uz from './uz.json';
 import ru from './ru.json';
 import en from './en.json';
 
-export const defaultLocale = 'uz';
+export const defaultLocale = 'en';
 export const languages = {
     uz,
     ru,
